81d7dee810e32b2bcfde062255be8d702d6f595f
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2020-05-26 18:03:35 -0500

    Se añade funcionalidad jugador 1 (Falta opción pedir carta)

--- 21.py
+++ 21.py
@@ -1,26 +1,42 @@
 from random import *
 
-def genera_mazo():
-    return [(numero,tipo) for numero in ['A','K','Q','J']+[x for x in range(2,11)] for tipo in ('corazones','picas','treboles','diamantes')]
+print("plantar, pedir")
+while True:
+    def genera_mazo():
+        return [(numero,tipo) for numero in ['A','K','Q','J']+[x for x in range(2,11)] for tipo in ('corazones','picas','treboles','diamantes')]
 
-def genera_carta(mazo):
-    return choice(mazo)
+    def genera_carta(mazo):
+        return choice(mazo)
 
-def limpia_mazo(carta,mazo):
-    mazo.remove(carta)
-    return carta
+    def limpia_mazo(carta,mazo):
+        mazo.remove(carta)
+        return carta
 
-def llena_jugador1(mazo):
-    return [x for x in range(0,2) for x in limpia_mazo(genera_carta(mazo), mazo)]
+    def llena_jugador1(mazo):
+        return [x for x in range(0,2) for x in limpia_mazo(genera_carta(mazo), mazo)]
 
-def equivalencia(cartas):
-    aux=0
-    for x in cartas:
-        if x=='K' or x=='Q' or x=='J':
-            cartas[aux]=10
-        elif x=='A':
-            cartas[aux]=11
-        aux+=1
-    return cartas
+    def equivalencia(cartas):
+        aux=0
+        for x in cartas:
+            if x=='K' or x=='Q' or x=='J':
+                cartas[aux]=10
+            elif x=='A':
+                cartas[aux]=11
+            aux+=1
+        return cartas
 
-print("Las dos cartas son: ",llena_jugador1(genera_mazo()))
+    def suma(cartas):
+        equivalencia(cartas)
+        return sum([x for x in cartas if cartas.index(x)%2==0]), cartas
+
+    def juega():
+        return llena_jugador1(genera_mazo())
+
+    print(suma(juega()))
+
+    if input()=="plantar":
+        break
+    elif input()=="pedir":
+        pass
+
+
